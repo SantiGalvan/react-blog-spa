d275af25929d052afa35c2c2f9534d5137c2fdc6
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-21 17:58:16 +0200

    added form create

--- src/pages/PostCreate.jsx
+++ src/pages/PostCreate.jsx
@@ -1,9 +1,35 @@
+import Form from "../components/Form/Form";
+import { useState, useEffect } from "react";
+import axios from "axios";
+const apiUrl = import.meta.env.VITE_BASE_API_URL;
+
 const PostCreate = () => {
+
+    const [tags, setTags] = useState([]);
+    const fetchTags = async () => {
+        const res = await axios.get(`${apiUrl}/tags`);
+        const apiTags = res.data;
+        setTags(apiTags);
+    }
+
+    const [categories, setCategories] = useState([]);
+    const fetchCategories = async () => {
+        const res = await axios.get(`${apiUrl}/categories`);
+        const apiCategories = res.data;
+        setCategories(apiCategories);
+    }
+
+    useEffect(() => {
+        fetchTags();
+        fetchCategories();
+    }, [])
+
     return (
         <section className="container">
-            <form>
-                form di creazione
-            </form>
+            <Form
+                tags={tags}
+                categories={categories}
+            />
         </section>
     )
 }
